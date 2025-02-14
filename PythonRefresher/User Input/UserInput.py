e1361c939fcdf45c19d5ef52e9235a13c8c96146
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2025-02-14 00:05:55 -0500

    commit

--- PythonRefresher/User Input/UserInput.py
+++ PythonRefresher/User Input/UserInput.py
@@ -3,6 +3,7 @@ User Input
 """
 
 first_name = input("Enter your first name: ")
+age = input("How old are you? ")
 days = input("How many days before your birthday: ")
 print(f"Hi {first_name}, only {days} days "
       f"before your birthday!")
---
+++ my.py
@@ -0,0 +1,6 @@
+# lsit in pythong 
+my_list =[80, 96, 72, 100, 8]
+print(my_list)
+
+people_list = ["John", "Jane", "Jim", "Jill"]
+print(people_list[-1])
